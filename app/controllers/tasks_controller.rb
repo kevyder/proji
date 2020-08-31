8178265d3ec2ab167e75346777661a3057505e4b
Kevin <kyrodriguez97@gmail.com>
2020-08-30 20:13:44 -0500

    Mark task as completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,18 @@
 class TasksController < ApplicationController
-  before_action :set_project, only: %i[create update destroy]
+  before_action :set_project
+  before_action :set_task, except: %i[create]
 
   def create
     @task = @project.tasks.create(tasks_params)
     redirect_to @project
   end
 
+  def complete
+    @task.update_attribute(:completed_at, Time.now) if @task.completed_at.nil?
+    redirect_to @project, notice: "Task completed."
+  end
+
   def destroy
-    @task = @project.tasks.find(params[:id])
     if @task.destroy
       flash[:success] = "Task was deleted."
     else
@@ -24,7 +29,9 @@ class TasksController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 
-  def set_task; end
+  def set_task
+    @task = @project.tasks.find(params[:id])
+  end
 
   def tasks_params
     params[:task].permit(:content)
